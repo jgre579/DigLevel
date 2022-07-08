ff8b3cc23ee147c309ff1e44f0d7ff92ba12d4da
jgre579 <jgre579@aucklanduni.ac.nz>
2022-07-08 23:29:44 +1200

    Add angle text views and calibration

--- app/src/main/java/com/example/diglevel/DeviceActivity.java
+++ app/src/main/java/com/example/diglevel/DeviceActivity.java
@@ -12,14 +12,11 @@ import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothProfile;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.RectF;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -27,10 +24,19 @@ import java.util.UUID;
 
 public class DeviceActivity extends AppCompatActivity {
     private UUID dataCharacteristic = UUID.fromString("0000ffe4-0000-1000-8000-00805f9a34fb");
+
+
+
     class ViewHolder {
         CustomBullseye bullseye;
+        TextView xTV, yTV, zTV, calibrateTV;
         public ViewHolder() {
             bullseye = findViewById(R.id.bullseye);
+            xTV = findViewById(R.id.angle_x_text);
+            yTV = findViewById(R.id.angle_y_text);
+            zTV = findViewById(R.id.angle_z_text);
+            calibrateTV = findViewById(R.id.calibrate_text);
+
 
 
         }
@@ -43,16 +49,10 @@ public class DeviceActivity extends AppCompatActivity {
         BluetoothDevice device = getIntent().getParcelableExtra("ble_device");
         vh = new ViewHolder();
 
-
-
-
         Bitmap dbitmap = BitmapFactory.decodeResource(getResources(), R.drawable.bullseye);
         Bitmap bitmap = dbitmap.copy(Bitmap.Config.ARGB_8888, true);
         vh.bullseye.setImageBitmap(bitmap);
         vh.bullseye.invalidate();
-
-
-
 
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -64,8 +64,39 @@ public class DeviceActivity extends AppCompatActivity {
 
     }
 
+    private double calibrationX, calibrationY = 0.0f;
+    float angleX, angleY, angleZ = 0;
+
+    public void clickCalibrate(View view) {
+
+        calibrationX = -angleX;
+        calibrationY = -angleY;
+
+    }
+
+    public void clickReset(View view) {
+
+        calibrationX = 0.0f;
+        calibrationY = 0.0f;
+    }
+
+
     private int h;
     private int w;
+    String deg = "\u00B0";
+
+    private double[] calibrate(double x, double y) {
+
+        double xOut = x + calibrationX;
+        double yOut = y + calibrationY;
+        vh.calibrateTV.setText("Current Calibration X : " + String.format("%.2f",calibrationX) + deg + " Y: " + String.format("%.2f",calibrationY) + deg );
+
+        double[] xy = {xOut, yOut};
+
+        return xy;
+
+
+    }
 
     private double[] transformAngleToScreen(double x, double y) {
 
@@ -78,7 +109,7 @@ public class DeviceActivity extends AppCompatActivity {
         double input_end = 90;
 
         //Used to emphasize inaccuracies, smaller angles make bigger changes to dot position.
-        double close_stretch = 2;
+        double close_stretch = 10;
 
         x = (x * close_stretch);
         y = (y * close_stretch);
@@ -99,7 +130,46 @@ public class DeviceActivity extends AppCompatActivity {
 
     }
 
+    private void newData(byte[] data) {
+        byte startByte = 0x55;
+        byte angleByte = 0x61;
+
+        if(data[0] == startByte && data[1] == angleByte) {
+
+            byte rollH = data[15];
+            byte rollL = data[14];
+
+            byte pitchH= data[17];
+            byte pitchL= data[16];
+
+            byte yawH = data[19];
+            byte yawL = data[18];
+
+            angleX = (((float) rollH * 256 + rollL)/32768)*180;
+            angleY = (((float) pitchH * 256 + pitchL)/32768)*180;
+            angleZ = (((float) yawH * 256 + yawL)/32768)*180;
+
+            double[] calxy = calibrate(angleX, angleY);
+            angleX = (float) calxy[0];
+            angleY = (float) calxy[1];
+
+
+
+
+            vh.xTV.setText("X: " + String.format("%.2f",angleX) + deg);
+            vh.yTV.setText("Y: " + String.format("%.2f", angleY) + deg);
+            vh.zTV.setText("Z: " + String.format("%.2f", angleZ) + deg);
+
+            Log.d("xyz", "X: " + String.valueOf(angleX) + " Y: " + String.valueOf(angleY) + " Z: " + String.valueOf(angleZ));
+            double[] xy = transformAngleToScreen(angleX, angleY);
+            vh.bullseye.setCircleXY((float)xy[0],(float)xy[1]);
+            vh.bullseye.invalidate();
+
+        }
+    }
+
     private String TAG = "gatt";
+
     BluetoothGattCallback bluetoothGattCallback =
             new BluetoothGattCallback() {
                 @SuppressLint("MissingPermission")
@@ -118,55 +188,22 @@ public class DeviceActivity extends AppCompatActivity {
 
                 @Override
                 public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
-                    Log.d(TAG,"Notify");
+                    //Log.d(TAG,"Notify");
                     super.onCharacteristicChanged(gatt, characteristic);
-                    byte startByte = 0x55;
-                    byte angleByte = 0x61;
-                    float angleX, angleY, angleZ = 0;
                     final byte[] data = characteristic.getValue();
+                    String x = "";
+                    for (int i = 0; i < data.length; i++) {
 
-                    Log.d(TAG, byteToHex(data[0]));
-                    Log.d(TAG, byteToHex(data[1]));
-
-                    if(data[0] == startByte && data[1] == angleByte) {
-
-                        byte rollH = data[15];
-                        byte rollL = data[14];
-
-                        byte pitchH= data[17];
-                        byte pitchL= data[16];
-
-                        byte yawH = data[19];
-                        byte yawL = data[18];
-
-                        Log.d(TAG, "Roll H: "+ rollH);
-                        Log.d(TAG, "Roll H b : "+ byteToHex(rollH));
-                        long t = (long) rollH * (256);
-                        long t1 = t + rollL;
-                        long t2 = t1/32768;
-                        long t3 = t2 * 180;
-                        angleX = (((float) rollH * 256 + rollL)/32768)*180;
-                        angleY = (((float) pitchH * 256 + pitchL)/32768)*180;
-                        angleZ = (((float) yawH * 256 + yawL)/32768)*180;
-
-//                            angleX = (((rollH<<8)|rollL)/32768) * 180;
-//                            angleY = ((pitchH<<8)|pitchL)/32768*180;
-//                            angleZ = ((yawH<<8)|yawL)/32768*180;
-
-                        Log.d("xyz", "X: " + String.valueOf(angleX) + " Y: " + String.valueOf(angleY) + " Z: " + String.valueOf(angleZ));
-                        double[] xy = transformAngleToScreen(angleX, angleY);
-                        vh.bullseye.setCircleXY((float)xy[0],(float)xy[1]);
-                        vh.bullseye.invalidate();
+                        x = x + byteToHex(data[i]) + " ";
 
                     }
 
+                    Log.d(TAG, x);
 
-                    for(byte b : data) {
 
-                        //Log.d(TAG, byteToHex(b));
-                        //Log.d(TAG, String.valueOf(b));
-                    }
                 }
+
+
 
                 public String byteToHex(byte num) {
                     char[] hexDigits = new char[2];
@@ -183,15 +220,17 @@ public class DeviceActivity extends AppCompatActivity {
                         for (BluetoothGattService service : services) {
                             List<BluetoothGattCharacteristic> characteristics = service.getCharacteristics();
                             for (BluetoothGattCharacteristic characteristic : characteristics) {
+
                                 ///Once you have a characteristic object, you can perform read/write
                                 //operations with it
                                 Log.d(TAG, characteristic.getUuid().toString());
-                                //gatt.readCharacteristic(characteristic);
+
+                               
 
 
                                 if(characteristic.getUuid().equals(dataCharacteristic)) {
                                     Log.d(TAG, "match found reading...");
-                                    //gatt.readCharacteristic(characteristic);
+                                    gatt.readCharacteristic(characteristic);
 
                                     gatt.setCharacteristicNotification(characteristic, true);
                                     BluetoothGattDescriptor descriptor = characteristic.getDescriptor(
@@ -202,12 +241,18 @@ public class DeviceActivity extends AppCompatActivity {
 
 
 
+
                                 }
                             }
 
 
                         }
                     }
+                }
+                @Override
+                public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+                    super.onCharacteristicWrite(gatt, characteristic, status);
+                    Log.d(TAG, "Characteristic " + characteristic.getUuid() + " written");
                 }
                 @Override
                 public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
